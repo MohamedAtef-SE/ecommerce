8c4ce489cca339957cc8204e17a2c62d57454dfd
mohamedatef-SE <mohamedatefyoussef@gmail.com>
2025-01-23 17:41:44 +0200

    Remove docs

--- docs/server/chunk-STLZQXAZ.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-var e={baseURL:"https://ecommerce.routemisr.com",urlServer:"http://localhost:4200"};export{e as a};
--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-6T2IWZ3K.mjs";import"./chunk-C6ZSU5B2.mjs";import"./chunk-E46HLUB7.mjs";import"./chunk-JXGVWCX3.mjs";import"./chunk-RCUM6EY4.mjs";import"./chunk-7HRUJERT.mjs";import"./chunk-VEAUVYHF.mjs";import"./chunk-W3SLRU5W.mjs";import"./chunk-JC33AN3A.mjs";import"./chunk-TUFFLGTC.mjs";import"./chunk-STLZQXAZ.mjs";import"./chunk-J4BDXTMW.mjs";import"./chunk-OPLI5KME.mjs";import"./chunk-RHIAHIG3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
